a94d0ffd9957cba7ddec7e243650d1c2dc318ade
unknown <rf505yt@gmail.com>
2025-07-26 03:25:40 +0200

    Fixing Services Functionalities

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const si = require('systeminformation');
 const { execSync } = require('child_process');
+const { exec } = require('child_process');
 
 const app = express();
 const PORT = 3000;
